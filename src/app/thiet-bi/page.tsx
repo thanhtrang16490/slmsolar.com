a1d93d5fd33a22bd0b4cdeb78637a51092e251e4
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-03-02 16:42:55 +0700

    update device
    update

--- src/app/thiet-bi/page.tsx
+++ src/app/thiet-bi/page.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Col, Divider, Layout, Row, Typography } from "antd";
+import { Col, Collapse, Divider, Layout, Row, Typography } from "antd";
 import { Content } from "antd/es/layout/layout";
-import { RightOutlined } from '@ant-design/icons';
+import { CaretRightOutlined, RightOutlined } from '@ant-design/icons';
 import Image from "next/image";
 
 
@@ -9,47 +9,71 @@ import Image from "next/image";
 const itemsSolarData = [
   {
     title: 'Pin mặt trời JA Solar',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
 
   },
   {
     title: 'Pin mặt trời Canadian Solar',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
   },
   {
     title: 'Pin mặt trời Risen Solar',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
   },
   {
     title: 'Pin mặt trời Longi Solar',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
-
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/solar-device.png' alt='device icon' width={40} height={65} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
   },
 ];
 
 const itemsInverterData = [
   {
     title: 'Inverter Deyee',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/inverter-deyee-device.png' alt='device icon' width={40} height={65} />
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/inverter-deyee-device.png' alt='device icon' width={40} height={65} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
   },
   {
     title: 'Inverter Luxpower ',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/inverter-luxpower-device.png' alt='device icon' width={40} height={62} />
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/inverter-luxpower-device.png' alt='device icon' width={40} height={62} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
   },
   {
     title: 'Inverter Huawei',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/inverter-huawei-device.png' alt='device icon' width={40} height={40} />
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/inverter-huawei-device.png' alt='device icon' width={40} height={40} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
   },
   {
     title: 'Inverter Growatt',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/inverter-growatt-device.png' alt='device icon' width={40} height={47} />
-
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/inverter-growatt-device.png' alt='device icon' width={40} height={47} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
 
   },
 ];
@@ -57,28 +81,42 @@ const itemsInverterData = [
 const itemsBatteryData = [
   {
     title: 'Pin lưu trữ lithium SMB',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/smb-device.png' alt='device icon' width={40} height={46} />
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/smb-device.png' alt='device icon' width={40} height={46} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
 
   },
   {
     title: 'Pin lưu trữ lithium UFO ',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/ufo-device.png' alt='device icon' width={40} height={59} />
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/ufo-device.png' alt='device icon' width={40} height={59} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
 
   },
   {
     title: 'Pin lưu trữ lithium SVE',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/sve-device.png' alt='device icon' width={40} height={18} />
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/sve-device.png' alt='device icon' width={40} height={18} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
 
   },
   {
     title: 'Pin lưu trữ lithium Larada',
-    icon: <Image style={{ position: 'absolute', left: '50%', top: '50%', translate: '-50% -50%' }} loading='eager' src='/img/nadara-device.png' alt='device icon' width={40} height={50} />
+    icon:
+      <div style={{ position: 'relative' }}>
+        <Image style={{ zIndex: '999', position: 'absolute', left: '15px', top: '15px', translate: '-50% -50%' }} loading='eager' src='/img/nadara-device.png' alt='device icon' width={40} height={50} />
+        <div style={{ left: '15px', top: '15px', translate: '-50% -50%', position: 'absolute', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}></div>
+      </div>
 
 
   },
 ];
-
-
 
 
 export default function ThietBi() {
@@ -87,8 +125,6 @@ export default function ThietBi() {
     <>
       <Layout style={{ width: '100%', background: '#fff' }}>
         <Content style={{ display: 'flex', flexDirection: 'column', width: '100%', alignContent: 'center' }}>
-
-
 
           <Row style={{ position: 'relative' }}>
 
@@ -100,106 +136,101 @@ export default function ThietBi() {
                 <Typography.Paragraph style={{ textAlign: 'left', margin: '20px 70px 10px 20px', fontSize: '14px', fontWeight: '300' }} >Solarmax cung cấp thiết bị năng lượng mặt trời đến từ các thương hiệu hàng đầu thế giới với chi phí và hiệu năng tối ưu giúp khách hàng trải có được trải nhiệm tốt và hài lòng nhất</Typography.Paragraph>
               </div>
             </Col>
-
           </Row>
 
           <Row>
             <Typography.Title style={{ paddingLeft: '25px' }} level={4}>Pin năng lượng mặt trời</Typography.Title>
             <Col style={{ width: '100%', margin: '0 10px 0', background: '#fff', borderRadius: '8px' }}>
               <Row>
+                {itemsSolarData.map((item, index) => {
+                  return (
+                    <>
+                      <Col style={{ width: '100%', borderRadius: '8px' }}>
+                        <Collapse
+                          size="large"
+                          items={[{
+                            key: '1',
+                            label: <Typography.Title style={{ margin: '20px 0 0 50px', height: '35px', color: '#000', textAlign: 'left', fontWeight: 400, fontSize: '14px' }} >{item.title}</Typography.Title>,
+                            children: <>{item.title}</>,
+                            extra: <RightOutlined style={{ margin: '20px 0 0 0px' }} />,
 
-                <Col style={{ width: '100%', padding: '10px', background: '#fff', borderRadius: '8px' }}>
-                  {itemsSolarData.map((item, index) => {
-                    return (
-                      <>
-                        <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                          <Row style={{ display: 'flex', alignItems: 'center' }}>
-                            <Col style={{ position: 'relative', borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}>{item.icon}</Col>
-                            <Col>
-                              <Typography.Title style={{ color: '#000', margin: '0px 5px 0px 5px', textAlign: 'left', fontWeight: 500 }} level={5}>{item.title}</Typography.Title>
-                            </Col>
-                          </Row>
-                          <Col>
-                            <RightOutlined />
-                          </Col>
-                        </Row>
-                        {index !== itemsSolarData.length - 1 && <Divider style={{ margin: '5px 0' }} />}
-                      </>
-                    )
-                  }
-                  )}
+                          }]}
+                          expandIcon={({ isActive }) => <>{item.icon}</>}
+                          style={{ background: '#ddd', margin: '5px 0', borderRadius: '6px' }}
+                        />
+                      </Col>
+                      {/* {index !== itemsSolarData.length - 1 && <Divider style={{ margin: '5px 0' }} />} */}
+                    </>
+                  )
+                }
+                )}
 
-                </Col>
+
               </Row>
-
-
 
             </Col>
           </Row>
+
           <Row>
             <Typography.Title style={{ paddingLeft: '25px' }} level={4}>Biến tần - Inverter</Typography.Title>
             <Col style={{ width: '100%', margin: '0 10px 0', background: '#fff', borderRadius: '8px' }}>
               <Row>
+                {itemsInverterData.map((item, index) => {
+                  return (
+                    <>
+                      <Col style={{ width: '100%', borderRadius: '8px' }}>
+                        <Collapse
+                          size="large"
+                          items={[{
+                            key: '1',
+                            label: <Typography.Title style={{ margin: '20px 0 0 50px', height: '35px', color: '#000', textAlign: 'left', fontWeight: 400, fontSize: '14px' }} >{item.title}</Typography.Title>,
+                            children: <>{item.title}</>,
+                            extra: <RightOutlined style={{ margin: '20px 0 0 0px' }} />,
 
-                <Col style={{ width: '100%', padding: '10px', background: '#fff', borderRadius: '8px' }}>
-                  {itemsInverterData.map((item, index) => {
-                    return (
-                      <>
-                        <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                          <Row style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-                            <Col style={{ borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}>{item.icon}</Col>
-                            <Col>
-                              <Typography.Title style={{ color: '#000', margin: '0px 5px 0px 5px', textAlign: 'left', fontWeight: 500 }} level={5}>{item.title}</Typography.Title>
-                            </Col>
-                          </Row>
-                          <Col>
-                            <RightOutlined />
-                          </Col>
-                        </Row>
-                        {index !== itemsInverterData.length - 1 && <Divider style={{ margin: '5px 0' }} />}
-                      </>
-                    )
-                  }
-                  )}
+                          }]}
+                          expandIcon={({ isActive }) => <>{item.icon}</>}
+                          style={{ background: '#ddd', margin: '5px 0', borderRadius: '6px' }}
+                        />
+                      </Col>
 
-                </Col>
+                    </>
+                  )
+                }
+                )}
+
               </Row>
-
-
 
             </Col>
           </Row>
 
           <Row>
-            <Typography.Title style={{ paddingLeft: '25px' }} level={4}>Pin Lithium</Typography.Title>
+            <Typography.Title style={{ paddingLeft: '25px' }} level={4}>Pin lưu trữ Lithium</Typography.Title>
             <Col style={{ width: '100%', margin: '0 10px 0', background: '#fff', borderRadius: '8px' }}>
               <Row>
+                {itemsBatteryData.map((item, index) => {
+                  return (
+                    <>
+                      <Col style={{ width: '100%', borderRadius: '8px' }}>
+                        <Collapse
+                          size="large"
+                          items={[{
+                            key: '1',
+                            label: <Typography.Title style={{ margin: '20px 0 0 50px', height: '35px', color: '#000', textAlign: 'left', fontWeight: 400, fontSize: '14px' }} >{item.title}</Typography.Title>,
+                            children: <>{item.title}</>,
+                            extra: <RightOutlined style={{ margin: '20px 0 0 0px' }} />,
 
-                <Col style={{ width: '100%', padding: '10px', background: '#fff', borderRadius: '8px' }}>
-                  {itemsBatteryData.map((item, index) => {
-                    return (
-                      <>
-                        <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                          <Row style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-                            <Col style={{ borderRadius: '6px', background: '#F2F1F6', width: '60px', height: '70px' }}>{item.icon}</Col>
-                            <Col>
-                              <Typography.Title style={{ color: '#000', margin: '0px 5px 0px 5px', textAlign: 'left', fontWeight: 500 }} level={5}>{item.title}</Typography.Title>
-                            </Col>
-                          </Row>
-                          <Col>
-                            <RightOutlined />
-                          </Col>
-                        </Row>
-                        {index !== itemsBatteryData.length - 1 && <Divider style={{ margin: '5px 0' }} />}
-                      </>
-                    )
-                  }
-                  )}
+                          }]}
+                          expandIcon={({ isActive }) => <>{item.icon}</>}
+                          style={{ background: '#ddd', margin: '5px 0', borderRadius: '6px' }}
+                        />
+                      </Col>
 
-                </Col>
+                    </>
+                  )
+                }
+                )}
+
               </Row>
-
-
 
             </Col>
           </Row>
