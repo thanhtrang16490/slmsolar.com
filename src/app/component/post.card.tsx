40325cd7d36dd0cad99855f539ff9827aae52a3b
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-03-06 19:36:01 +0700

    update Mega Story

--- src/app/component/post.card.tsx
+++ src/app/component/post.card.tsx
@@ -15,9 +15,6 @@ type PostCardProps = {
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
     return (
         <>
-
-
-
             <Content style={{ padding: '6px 5px 5px' }}>
                 <Card
                     style={{ width: '100%', height: '100%', position: 'relative', boxShadow: '0 1px 2px 0 rgba(60,64,67,.1), 0 2px 6px 2px rgba(60,64,67,.15)' }}
@@ -27,7 +24,6 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
                                 <Image
                                     layout="fill"
                                     objectFit="cover"
-
                                     src={
                                         post.attributes.image.data && post.attributes.image.data.attributes.formats
                                             ? post.attributes.image.data.attributes.formats.medium
@@ -37,14 +33,12 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
                                                     : `${process.env.NEXT_PUBLIC_BACKEND_URL}${post.attributes.image.data.attributes.url}`
                                             : "/img/place-holder.png"
                                     }
-
                                     alt={post.attributes.title}
                                 />
                             </div>
                         </Link>
                     }
                 >
-
                     <Meta
                         description={
                             <Link href={`/blog/${post?.attributes.slug}`}>
@@ -56,9 +50,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
                     />
                 </Card>
             </Content>
-
         </>
-
     );
 
 }
